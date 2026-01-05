680a43afe8390e17bbc06be2a28f2754950c9dfa
selHasAMug <selyym1@gmail.com>
2026-01-05 07:09:53 -0700

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,34 @@
+const images = document.querySelectorAll('.scroller img');
+const left = document.querySelector('.arrow-left');
+const right = document.querySelector('.arrow-right');
+let x = 0;
+
+function updateScale() {
+    const center = window.innerWidth / 2;
+
+    images.forEach(img => {
+        const rect = img.getBoundingClientRect();
+        const imgCenter = rect.left + rect.width / 2;
+        const distance = Math.abs(center - imgCenter);
+        img.style.opacity = 1 - (distance ** 3 * 0.0000000043);
+
+        const scale = Math.max(0.75, 1.15 - distance / 2000);
+        img.style.transform =  `translateX(${x}px) scale(${scale})`
+    });
+
+    requestAnimationFrame(updateScale);
+}
+
+updateScale();
+
+function moveImages(direction) {
+    x += 294 * direction;
+    x = Math.min(1176, Math.max(x, -1176))
+
+    images.forEach(img => {
+        img.style.transform = `translateX(${x}px)`;
+    });
+}
+
+left.addEventListener('click', () => moveImages(1));
+right.addEventListener('click', () => moveImages(-1));
